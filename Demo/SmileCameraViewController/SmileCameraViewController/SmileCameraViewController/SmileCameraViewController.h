9ea3792ef1fb6951a9b851db14f4d990cdf19ce4
maxim.makhun <maxim.makhun@gmail.com>
2014-05-07 00:30:53 +0300

    Added view for smile detection.

--- Demo/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController.h
+++ Demo/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController.h
@@ -1,6 +1,6 @@
 //
-//  MMAViewController.h
-//  SmileCameraViewController
+//  SmileCameraViewController.h
+//  SmileCameraViewControllerDemo
 //
 //  Created by maxim.makhun on 5/6/14.
 //  Copyright (c) 2014 MMA. All rights reserved.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MMAViewController : UIViewController
+@interface SmileCameraViewController : UIViewController
 
 @end
---
+++ Demo/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController.m
@@ -0,0 +1,38 @@
+//
+//  SmileCameraViewController.m
+//  SmileCameraViewControllerDemo
+//
+//  Created by maxim.makhun on 5/6/14.
+//  Copyright (c) 2014 MMA. All rights reserved.
+//
+
+#import "SmileCameraViewController.h"
+
+@interface SmileCameraViewController ()
+
+@end
+
+@implementation SmileCameraViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
