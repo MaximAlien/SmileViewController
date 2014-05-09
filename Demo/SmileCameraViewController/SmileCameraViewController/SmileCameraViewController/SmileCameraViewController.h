a26a07356297140fe6fb37e9be90d23e45a3d156
maxim.makhun <maxim.makhun@gmail.com>
2014-05-09 17:47:17 +0300

    Refactored view controller and added resources. Moved UI to xib file.

--- Demo/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController.h
+++ Demo/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController.h
@@ -1,13 +1,13 @@
 //
-//  SmileCameraViewController.h
+//  SmileImageViewController.h
 //  SmileCameraViewControllerDemo
 //
-//  Created by maxim.makhun on 5/6/14.
+//  Created by maxim.makhun on 5/9/14.
 //  Copyright (c) 2014 MMA. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface SmileCameraViewController : UIViewController
+@interface SmileImageViewController : UIViewController
 
 @end
--- Demo/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController.m
+++ Demo/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController/SmileCameraViewController.m
@@ -1,18 +1,18 @@
 //
-//  SmileCameraViewController.m
+//  SmileImageViewController.m
 //  SmileCameraViewControllerDemo
 //
-//  Created by maxim.makhun on 5/6/14.
+//  Created by maxim.makhun on 5/9/14.
 //  Copyright (c) 2014 MMA. All rights reserved.
 //
 
-#import "SmileCameraViewController.h"
+#import "SmileImageViewController.h"
 
-@interface SmileCameraViewController ()
+@interface SmileImageViewController ()
 
 @end
 
-@implementation SmileCameraViewController
+@implementation SmileImageViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
