32da1b0dfdad25a72fcd844b5506b96357ae57ed
Maxim Makhun <maxim.makhun@gmail.com>
2017-10-19 22:22:30 +0200

    Remove unused test code.

--- SmileViewController/SmileViewController.m
+++ SmileViewController/SmileViewController.m
@@ -14,16 +14,11 @@
 // Categories
 #import "UIImage+Additions.h"
 #import "UIAlertController+Utilities.h"
-#import "CIFaceFeature+Helpers.h"
 
 @interface SmileViewController () <AVCaptureVideoDataOutputSampleBufferDelegate>
 
 @property(nonatomic, strong) AVCaptureVideoPreviewLayer *captureVideoPreviewLayer;
 @property(nonatomic, strong) UIImage *takenPhotoImage;
-@property(nonatomic, strong) CALayer *rightEyeLayer;
-@property(nonatomic, strong) CALayer *leftEyeLayer;
-@property(nonatomic, strong) CALayer *mouthLayer;
-@property(nonatomic, strong) CALayer *faceLayer;
 @property(nonatomic, weak) IBOutlet UIView *previewView;
 @property(nonatomic, weak) IBOutlet UIButton *retakePhotoButton;
 @property(nonatomic, weak) IBOutlet UIButton *showFrontCameraButton;
@@ -110,45 +105,7 @@
     }
 }
 
-- (IBAction)showFrontCamera:(id)sender {
-    AVCaptureSession *captureSession = self.captureVideoPreviewLayer.session;
-    AVCaptureDeviceInput *deviceInput = captureSession.inputs[0];
-    AVCaptureDevice *captureDevice;
-    
-    switch (deviceInput.device.position) {
-        case AVCaptureDevicePositionBack:
-            captureDevice = [AVCaptureDevice defaultDeviceWithDeviceType:AVCaptureDeviceTypeBuiltInWideAngleCamera
-                                                               mediaType:AVMediaTypeVideo
-                                                                position:AVCaptureDevicePositionFront];
-            break;
-        case AVCaptureDevicePositionFront:
-            captureDevice = [AVCaptureDevice defaultDeviceWithDeviceType:AVCaptureDeviceTypeBuiltInWideAngleCamera
-                                                               mediaType:AVMediaTypeVideo
-                                                                position:AVCaptureDevicePositionBack];
-            break;
-        case AVCaptureDevicePositionUnspecified:
-        default:
-            break;
-    }
-    
-    [captureSession beginConfiguration];
-    AVCaptureDeviceInput *newInput = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:nil];
-    [captureSession removeInput:deviceInput];
-    [captureSession addInput:newInput];
-    [captureSession commitConfiguration];
-}
-
-#pragma mark - Styling methods
-
-- (void)styleButtons {
-    self.retakePhotoButton.imageView.contentMode = UIViewContentModeScaleAspectFit;
-    [self.retakePhotoButton setImage:[UIImage imageNamed:@"retake_image"] forState:UIControlStateNormal];
-    [self.retakePhotoButton setTintColor:[UIColor whiteColor]];
-    
-    self.showFrontCameraButton.imageView.contentMode = UIViewContentModeScaleAspectFit;
-    [self.showFrontCameraButton setImage:[UIImage imageNamed:@"switch_camera_image"] forState:UIControlStateNormal];
-    [self.showFrontCameraButton setTintColor:[UIColor whiteColor]];
-}
+#pragma mark - AVCaptureVideoDataOutputSampleBufferDelegate methods
 
 - (void)captureOutput:(AVCaptureOutput *)captureOutput
 didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
@@ -175,105 +132,61 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
     
     if (features.count == 0) {
         NSLog(@"No face features available.");
-        
-        dispatch_async(dispatch_get_main_queue(), ^(void) {
-            [self.rightEyeLayer removeFromSuperlayer];
-            [self.leftEyeLayer removeFromSuperlayer];
-            [self.mouthLayer removeFromSuperlayer];
-            [self.faceLayer removeFromSuperlayer];
-        });
-        
         return;
     }
     
     for (CIFaceFeature *faceFeature in features) {
-        dispatch_async(dispatch_get_main_queue(), ^(void) {
-            [self.rightEyeLayer removeFromSuperlayer];
+        if (faceFeature.hasSmile) {
+            dispatch_async(dispatch_get_main_queue(), ^(void) {
+                UIImage *image = [[UIImage alloc] initWithCIImage:ciImage];
+                self.takenPhotoImage = image;
+            });
             
-            if (faceFeature.hasRightEyePosition) {
-                self.rightEyeLayer = [CALayer layer];
-                self.rightEyeLayer.frame = CGRectMake([faceFeature rightEyePositionForImage:uiImage size:self.previewView.frame.size].x,
-                                                      [faceFeature rightEyePositionForImage:uiImage size:self.previewView.frame.size].y,
-                                                      5.0f,
-                                                      5.0f);
-                self.rightEyeLayer.backgroundColor = [UIColor greenColor].CGColor;
-                self.rightEyeLayer.cornerRadius = CGRectGetWidth(self.leftEyeLayer.frame) / 2.0f;
-                
-                [self.previewView.layer addSublayer:self.rightEyeLayer];
-            }
-        });
-        
-        dispatch_async(dispatch_get_main_queue(), ^(void) {
-            [self.leftEyeLayer removeFromSuperlayer];
-            
-            if (faceFeature.hasLeftEyePosition) {
-                self.leftEyeLayer = [CALayer layer];
-                self.leftEyeLayer.frame = CGRectMake([faceFeature leftEyePositionForImage:uiImage size:self.previewView.frame.size].x,
-                                                     [faceFeature leftEyePositionForImage:uiImage size:self.previewView.frame.size].y,
-                                                     5.0f,
-                                                     5.0f);
-                self.leftEyeLayer.backgroundColor = [UIColor redColor].CGColor;
-                self.leftEyeLayer.cornerRadius = CGRectGetWidth(self.leftEyeLayer.frame) / 2.0f;
-                
-                [self.previewView.layer addSublayer:self.leftEyeLayer];
-            }
-        });
-        
-        dispatch_async(dispatch_get_main_queue(), ^(void) {
-            [self.mouthLayer removeFromSuperlayer];
-            
-            if (faceFeature.hasMouthPosition) {
-                self.mouthLayer = [CALayer layer];
-                self.mouthLayer.frame = CGRectMake([faceFeature mouthPositionForImage:uiImage size:self.previewView.frame.size].x,
-                                                   [faceFeature mouthPositionForImage:uiImage size:self.previewView.frame.size].y,
-                                                   5.0f,
-                                                   5.0f);
-                self.mouthLayer.backgroundColor = [UIColor blueColor].CGColor;
-                self.mouthLayer.cornerRadius = CGRectGetWidth(self.mouthLayer.frame) / 2.0f;
-                
-                [self.previewView.layer addSublayer:self.mouthLayer];
-            }
-        });
-        
-        // in case if user is smiling we stop any updates
-        dispatch_async(dispatch_get_main_queue(), ^(void) {
-            if (faceFeature.hasSmile) {
-                dispatch_async(dispatch_get_main_queue(), ^(void) {
-                    UIImage *image = [[UIImage alloc] initWithCIImage:ciImage];
-                    self.takenPhotoImage = image;
-                });
-                
-                [[self.captureVideoPreviewLayer session] stopRunning];
-            }
-        });
-        
-        dispatch_async(dispatch_get_main_queue(), ^(void) {
-            [self.faceLayer removeFromSuperlayer];
-            
-            self.faceLayer = [CALayer layer];
-            CGRect bounds = [faceFeature boundsForImage:uiImage size:self.previewView.frame.size];
-            self.faceLayer.frame = CGRectMake(bounds.origin.x,
-                                              bounds.origin.y,
-                                              bounds.size.width,
-                                              bounds.size.height);
-            self.faceLayer.backgroundColor = [UIColor clearColor].CGColor;
-            self.faceLayer.borderColor = [UIColor blackColor].CGColor;
-            self.faceLayer.borderWidth = 3.0f;
-            self.faceLayer.transform = CATransform3DMakeRotation(faceFeature.faceAngle / 180.0 * M_PI, 0.0, 0.0, 1.0);
-            
-            CALayer *glassesLayer = [CALayer layer];
-            glassesLayer.frame = CGRectMake(0,
-                                            [faceFeature leftEyePositionForImage:uiImage size:self.previewView.frame.size].y - bounds.origin.y - 40,
-                                            bounds.size.width,
-                                            bounds.size.height / 3);
-            
-            glassesLayer.contents = (id)[UIImage imageNamed:@"glasses_image"].CGImage;
-            glassesLayer.contentsGravity = kCAGravityResizeAspect;
-            [self.faceLayer addSublayer:glassesLayer];
-            
-            [self.previewView.layer addSublayer:self.faceLayer];
-        });
+            [[self.captureVideoPreviewLayer session] stopRunning];
+        }
     }
+}
+
+#pragma mark - Styling methods
+
+- (void)styleButtons {
+    self.retakePhotoButton.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    [self.retakePhotoButton setImage:[UIImage imageNamed:@"retake_image"] forState:UIControlStateNormal];
+    [self.retakePhotoButton setTintColor:[UIColor whiteColor]];
+    
+    self.showFrontCameraButton.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    [self.showFrontCameraButton setImage:[UIImage imageNamed:@"switch_camera_image"] forState:UIControlStateNormal];
+    [self.showFrontCameraButton setTintColor:[UIColor whiteColor]];
+}
+
+#pragma mark - Action handlers
+
+- (IBAction)showFrontCamera:(id)sender {
+    AVCaptureSession *captureSession = self.captureVideoPreviewLayer.session;
+    AVCaptureDeviceInput *deviceInput = captureSession.inputs[0];
+    AVCaptureDevice *captureDevice;
+    
+    switch (deviceInput.device.position) {
+        case AVCaptureDevicePositionBack:
+            captureDevice = [AVCaptureDevice defaultDeviceWithDeviceType:AVCaptureDeviceTypeBuiltInWideAngleCamera
+                                                               mediaType:AVMediaTypeVideo
+                                                                position:AVCaptureDevicePositionFront];
+            break;
+        case AVCaptureDevicePositionFront:
+            captureDevice = [AVCaptureDevice defaultDeviceWithDeviceType:AVCaptureDeviceTypeBuiltInWideAngleCamera
+                                                               mediaType:AVMediaTypeVideo
+                                                                position:AVCaptureDevicePositionBack];
+            break;
+        case AVCaptureDevicePositionUnspecified:
+        default:
+            break;
+    }
+    
+    [captureSession beginConfiguration];
+    AVCaptureDeviceInput *newInput = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:nil];
+    [captureSession removeInput:deviceInput];
+    [captureSession addInput:newInput];
+    [captureSession commitConfiguration];
 }
 
 - (IBAction)retakePhotoButtonPressed:(id)sender {
