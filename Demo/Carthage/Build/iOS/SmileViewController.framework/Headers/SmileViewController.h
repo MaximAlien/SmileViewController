b827b6ec0572cf2a4df2bab29a67fd39be823031
Maxim Makhun <maxim.makhun@gmail.com>
2017-11-27 22:38:17 +0100

    Add shared framework build target.

---
+++ Demo/Carthage/Build/iOS/SmileViewController.framework/Headers/SmileViewController.h
@@ -0,0 +1,19 @@
+//
+//  SmileViewController.h
+//  SmileViewController
+//
+//  Created by Maxim Makhun on 11/27/17.
+//  Copyright © 2017 Maxim Makhun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SmileViewController.
+FOUNDATION_EXPORT double SmileViewControllerVersionNumber;
+
+//! Project version string for SmileViewController.
+FOUNDATION_EXPORT const unsigned char SmileViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmileViewController/PublicHeader.h>
+
+
